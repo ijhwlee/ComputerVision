abb2813823c3c01ccf8fd3d5ec69a3019f081d4c
Lee Hyung Won <ijhwlee@gmail.com>
2023-12-28 18:22:44 +0900

    add new files

---
+++ y2024/OpenCV-Python/chap02/hello.py
@@ -0,0 +1,4 @@
+#hellp.py
+import cv2 as cv
+print("Hello")
+print(cv.__version__)
---
+++ y2024/OpenCV-Python/chap03/variable.py
@@ -0,0 +1,17 @@
+variable1 = 100
+variable2 = 3.14
+variable3 = -200
+variable4 = 1.2 + 3.4j
+variable5 = 'This is Python example'
+variable6 = True
+variable7 = float(variable1)
+variable8 = int(variable2)
+
+print('variable1 =', variable1, type(variable1))
+print('variable2 = {0}, type = {1}'.format(variable2, type(variable2)))
+print('variable3 = {0}, type = {1}'.format(variable3, type(variable3)))
+print('variable4 = {0}, type = {1}'.format(variable4, type(variable4)))
+print('variable5 = {0}, type = {1}'.format(variable5, type(variable5)))
+print('variable6 = {0}, type = {1}'.format(variable6, type(variable6)))
+print('variable7 = {0}, type = {1}'.format(variable7, type(variable7)))
+print('variable8 = {0}, type = {1}'.format(variable8, type(variable8)))
